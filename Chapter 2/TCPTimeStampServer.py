8511f049bf5fa9206547f517a8518e1ae7aedfe3
vishwanath79 <vishwanath79@gmail.com>
2014-03-09 13:46:04 -0400

    TCPTimeStampServer

---
+++ Chapter 2/TCPTimeStampServer.py
@@ -0,0 +1,24 @@
+# Create a TCP server that accepts messages from clients and returns them with timestamp prefix
+
+#Import socket attributes
+from socket import *
+from time import ctime
+HOST = '' # Indication to bind method so it can use any avail addr
+PORT = 21567 # Random port number
+BUFSIZ = 1024
+ADDR = (HOST,PORT)
+tcpSerSock = socket(AF_INET,SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5) # Max # of incoming connections before refusals
+while True:
+    print 'Waiting for connection'
+    tcpCliSock,addr = tcpSerSock.accept() 
+    print '...connected from: ', addr
+    # Wait for data
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break #break and close connection if message is blank
+        tcpCliSock.send('[%s] %s' %(ctime(),data)) # Prepend time stamp
+        tcpCliSock.close()
+    tcpSerSock.close()
