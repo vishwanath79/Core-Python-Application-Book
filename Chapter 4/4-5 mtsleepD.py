a6ced09e784ffa80d0c75ede4e86df2b9bf75647
Ingvay7 <vishwanath79 at outlook dot com>
2014-04-02 23:41:46 -0400

    Using callable classes

---
+++ Chapter 4/4-5 mtsleepD.py
@@ -0,0 +1,35 @@
+#  4-4 Using Callable Classes
+import threading
+from time import sleep,ctime
+loops = [4,2]
+class ThreadFunc(object):
+    def __init__(self,func,args,name=''): # set values
+        self.name =name
+        self.func=func
+        self.args=args
+        
+    def __call__(self):
+        self.func(*self.args)
+        
+    def loop(nloop,nsec):
+        print 'start loop',nloop,'at:',ctime()
+        sleep(nsec)
+        print 'loop',nloop,'done at:',ctime()
+        
+    def main():
+        print 'starting at:',ctime()
+        threads=[]
+        nloops=range(len(loops))
+        
+        for i in nloops: #create all threads
+            t=threading.Thread(
+              target=ThreadFunc(loop,(i,loops[i]),loop.__name__))
+            threads.append(t)
+            
+        for i in nloops: #start all threads
+            threads[i].start()
+            
+        for i in nloops: #wait for completion
+            threads[i].join()
+            
+        print 'all DONE at: ', ctime()
